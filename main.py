751662fb08f73afd4249e3a44d715de5eca53838
Anastasiia-Rokytska <anastasiia.rokytska.knm.2019@lpnu.ua>
2020-11-03 19:08:53 +0200

    create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/api/v1/hello-world-<id>", methods=["GET"])
+    def printHello(id):
+        return "Hello World "+id
+
+    return app
